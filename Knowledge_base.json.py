08bf12a039996d71662652ac782ba4bed24ca8ec
Victorrbuilds <kipvictorr01@gmail.com>
2026-09-07 13:13:41 +0300

    Update Knowledge_base.json.py

--- Knowledge_base.json.py
+++ Knowledge_base.json.py
@@ -1,1 +1,68 @@
-[]
+{
+  "qa_pairs": [
+    {
+      "question": "what is your name",
+      "answer": "I am your chatbot assistant."
+    },
+    {
+      "question": "Who are you",
+      "answer": "I am a chatbot designed to help answer questions?"
+    },
+    {
+      "question": "what is python",
+      "answer": "Python is a popular high-level programming language known for its readability and versatility."
+    },
+    {
+      "question": "what can you do",
+      "answer": "I can answer questions from my knowledge base, and if I don't know something, you can teach me the answerI can answer questions, provide information, and help with common tasks.."
+    },
+    {
+      "question": "i have a question",
+      "answer": "Tell me i'm here to help you with anything?"
+    },
+    {
+      "question": "name",
+      "answer": "seems your question is incomplete, check again and clarify so that i can help you"
+    },
+    {
+      "question": "who is mercy",
+      "answer": "Mercy is a nurse who works in Eldoret"
+    },
+    {
+      "question": "hi",
+      "answer": "Hi, how may I help you today"
+    },
+    {
+      "question": "what is thyranoid",
+      "answer": "The thyroid is a small, butterfly-shaped gland located at the base of the neck, just below the Adam's apple. As a vital part of the endocrine system, it produces hormones that regulate the body's metabolism, heart rate, temperature, and energy level"
+    },
+    {
+      "question": "How do I use this chatbot?",
+      "answer": "Just type your question, and I’ll do my best to help."
+    },
+    {
+      "question": "Are you a human?",
+      "answer": "No, I am an AI chatbot."
+    },
+    {
+      "question": "Can you help me?",
+      "answer": "Yes, I’m here to help."
+    },
+    {
+      "question": "how are you",
+      "answer": "I'm fine how may I help you today."
+    },
+    {
+      "question": "how are you doing",
+      "answer": "I'm doing well, how may i help ypu today?"
+    },
+    {
+      "question": "What is the capital of Kenya?",
+      "answer": "Nairobi."
+    },
+    {
+      "question": "who is the president of kenya",
+      "answer": "Dr. William Samoei Ruto"
+    }
+  ]
+}
